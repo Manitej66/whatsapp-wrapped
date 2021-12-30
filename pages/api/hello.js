02d11e0beb791fbdd97da8fbdf46d2037730aec8
Maniteja Pratha <pratamaniteja66@gmail.com>
2021-12-30 19:34:07 +0530

    initial

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const { file } = req.body;
+
+  const words = file.split("\n");
+  const wordCount = {};
+  words.forEach((word) => {
+    if (wordCount[word]) {
+      wordCount[word]++;
+    } else {
+      wordCount[word] = 1;
+    }
+  });
+  const max = Math.max(...Object.values(wordCount));
+  const mostUsedWord = Object.keys(wordCount).find(
+    (word) => wordCount[word] === max
+  );
+
+  res.status(200).json({
+    word: mostUsedWord,
+    count: max,
+  });
 }
